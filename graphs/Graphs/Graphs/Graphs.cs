77708f314d21a5af7cf6a91cd9290fb8f42727e4
teejlaweej <josephtcortez@gmail.com>
2020-05-19 22:03:08 -0700

    DFS and BFS c#

---
+++ graphs/Graphs/Graphs/Graphs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Graphs
+{
+    public class Graphs
+    {
+        public Node[] nodes;
+        public class Node {
+            public string name;
+            public List<Node> children;
+            public bool visited;
+
+            public Node(string val)
+            {
+                this.name = val;
+                this.children = new List<Node>();
+                this.visited = false;
+            }
+            
+        }
+        public void DFS(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            //current node now visited
+            root.visited = true;
+            Console.WriteLine(root.name);
+            //go through each node's children, if haven't been seen recurse
+            foreach (Node child in root.children)
+            {
+                if (child.visited == false)
+                {
+                    DFS(child);
+                }
+            }
+        }
+
+        public void BFS(Node root)
+        {
+            Queue<Node> queue = new Queue<Node>();
+            root.visited = true;
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node cur = queue.Dequeue();
+                Console.WriteLine(cur.name);
+                foreach (Node child in cur.children)
+                {
+                    if (child.visited == false)
+                    {
+                        child.visited = true;
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ graphs/Graphs/Graphs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Graphs.Graphs;
+
+namespace Graphs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Graphs graph = new Graphs();
+            Node oneNode = new Node("0");
+            Node twoNode = new Node("1");
+            Node threeNode = new Node("2");
+            Node fourNode = new Node("3");
+            Node fiveNode = new Node("4");
+            Node sixNode = new Node("5");
+            Node sevenNode = new Node("6");
+            Node eighthNode = new Node("7");
+            Node nineNode = new Node("8");
+
+            oneNode.children.Add(twoNode);
+            oneNode.children.Add(fourNode);
+            oneNode.children.Add(sevenNode);
+            //Console.WriteLine(oneNode.children[1].name);
+            twoNode.children.Add(threeNode);
+            threeNode.children.Add(fourNode);
+            threeNode.children.Add(fiveNode);
+            fourNode.children.Add(sixNode);
+            fiveNode.children.Add(sixNode);
+            fiveNode.children.Add(nineNode);
+            sevenNode.children.Add(nineNode);
+            nineNode.children.Add(eighthNode);
+
+            graph.DFS(oneNode);
+            //graph.BFS(oneNode);
+        }
+    }
+}
