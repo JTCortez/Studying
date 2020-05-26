7b5eb8cde83fe8e2970a0719535ecf89e2d66cb5
teejlaweej <josephtcortez@gmail.com>
2020-05-25 21:10:58 -0700

    merge sort

---
+++ Sorting/Sorting/MergeSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    class MergeSort
+    {
+        public List<int> MergeSortHelper(List<int> array)
+        {
+            if (array.Count == 1)
+                return array;
+
+            
+            List<int> arrayOne = new List<int>(array.GetRange(0, array.Count/2));
+            List<int> arrayTwo = array.GetRange(array.Count / 2, array.Count / 2);
+
+            arrayOne = MergeSortHelper(arrayOne);
+            arrayTwo = MergeSortHelper(arrayTwo);
+
+            return Merge(arrayOne, arrayTwo);
+        }
+
+        public List<int> Merge(List<int> a, List<int> b)
+        {
+            List<int> c = new List<int>(a.Count + b.Count);
+
+            int aIndex = 0;
+            int bIndex = 0;
+            int cIndex = 0;
+
+            while (aIndex <= a.Count - 1 && bIndex <= b.Count - 1)
+            {
+                if (a[aIndex] > b[bIndex])
+                {
+                    c.Add(b[bIndex]);
+                    bIndex += 1;
+                }
+                else
+                {
+                    c.Add(a[aIndex]);
+                    aIndex += 1;
+                }
+            }
+
+            while (aIndex <= a.Count - 1)
+            {
+                c.Add(a[aIndex]);
+                aIndex += 1;
+            }
+
+            while (bIndex <= b.Count - 1)
+            {
+                c.Add(b[bIndex]);
+                bIndex += 1;
+            }
+            return c;
+        }
+    }
+}
--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using static Sorting.BubbleSort;
 using static Sorting.SelectionSort;
+using static Sorting.MergeSort;
 
 namespace Sorting
 {
@@ -8,26 +10,37 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            BubbleSort bsort = new BubbleSort();
-            int[] unsorted = { 3, 6, 4, 2, 5, 1, 2, 7, 10, 9, 7, 8, 11, 12, 3 };
-            bsort.BubbleSortAlg(unsorted);
+            //BubbleSort bsort = new BubbleSort();
+            //int[] unsorted = { 3, 6, 4, 2, 5, 1, 2, 7, 10, 9, 7, 8, 11, 12, 3 };
+            //bsort.BubbleSortAlg(unsorted);
 
-            SelectionSort sSort = new SelectionSort();
-            int[] unsorted2 = { 3, 6, 4, 2, 5, 1, 2, 7, 10, 9, 7, 8, 11, 12, 3 };
-            sSort.SelectionSortAlg(unsorted2);
+            //SelectionSort sSort = new SelectionSort();
+            //int[] unsorted2 = { 3, 6, 4, 2, 5, 1, 2, 7, 10, 9, 7, 8, 11, 12, 3 };
+            //sSort.SelectionSortAlg(unsorted2);
 
-            foreach (int i in unsorted)
+            MergeSort mSort = new MergeSort();
+            int[] unsorted3 = { 2, 8, 5, 3, 9, 4, 1, 7 };
+            List<int> unsortedlist = new List<int>(unsorted3);
+            List<int> sorted = mSort.MergeSortHelper(unsortedlist);
+
+
+            foreach (int i in sorted)
             {
                 Console.Write(i + ",");
             }
 
 
-            Console.WriteLine(" ");
+            //Console.WriteLine(" ");
 
-            foreach (int i in unsorted2)
-            {
-                Console.Write(i + ",");
-            }
+            //foreach (int i in unsorted2)
+            //{
+            //    Console.Write(i + ",");
+            //}
+
+            //foreach (int i in sorted)
+            //{
+            //    Console.Write(i + ",");
+            //}
             return;
         }
     }
