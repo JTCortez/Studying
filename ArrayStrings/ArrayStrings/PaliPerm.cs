67c29a3307524520162e4024becfb47903ace5a1
teejlaweej <josephtcortez@gmail.com>
2020-05-26 16:33:56 -0700

    Palindrome Permutation

---
+++ ArrayStrings/ArrayStrings/PaliPerm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayStrings
+{
+    class PaliPerm
+    {
+        public bool PalindromePermutation(string phrase)
+        {
+            Hashtable strTable = new Hashtable();
+            int maxOdd = 1;
+            foreach (char c in phrase.ToLower())
+            {
+                if (c == ' ') continue;
+                if (strTable.ContainsKey(c) == false)
+                {
+                    strTable.Add(c, 1);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    strTable[c] = (int)strTable[c] + 1;
+                }
+            }
+
+            foreach (int num in strTable.Values)
+            {
+                if (num % 2 == 1)
+                {
+                    maxOdd -= 1;
+                }
+            }
+
+            return maxOdd >= 0 ? true: false;
+        }
+    }
+}
---
+++ ArrayStrings/ArrayStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static ArrayStrings.PaliPerm;
+namespace ArrayStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaliPerm pp = new PaliPerm();
+
+            Console.WriteLine(pp.PalindromePermutation("Tacto Coasf"));
+        }
+    }
+}
