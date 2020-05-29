1cd8119f511f86d2b2e328a6f34fc23128f679a2
teejlaweej <josephtcortez@gmail.com>
2020-05-29 15:56:27 -0700

    dynamic programming, triplestep

---
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static DynamicProgramming.TripleStep;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            TripleStep dp = new TripleStep();
+            int ans = dp.TripleStepFunction(n);
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ DynamicProgramming/DynamicProgramming/TripleStep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    class TripleStep
+    {
+        public int TripleStepFunction(int n)
+        {
+            return TSHelp(n, new int[n + 1]);
+        }
+
+        public int TSHelp (int n, int[] memo)
+        {
+            //27, different because I included extra step, 4 steps is 5
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 3;
+            if (memo[n] == 0)
+            {
+                memo[n] = TSHelp(n - 1, memo) + TSHelp(n - 2, memo) + TSHelp(n - 3, memo);
+            }
+
+            return memo[n];
+        }
+    }
+}
